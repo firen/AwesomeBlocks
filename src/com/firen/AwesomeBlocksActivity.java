9dae12ac4e1194d83bb68aaf793e168ee624baa0
firen@firen-PC <none@none>
2011-10-09 17:07:40 +0200

    init commit

---
+++ src/com/firen/AwesomeBlocksActivity.java
@@ -0,0 +1,13 @@
+package com.firen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AwesomeBlocksActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

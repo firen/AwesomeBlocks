0af729862e193b8ca55d6da6f30e0fd826a383a1
firen@firen-PC <none@none>
2011-10-09 21:27:50 +0200

    add selection of blocks and their neighbourse with that same color

--- src/com/firen/awesomeblocks/Board.java
+++ src/com/firen/awesomeblocks/Board.java
@@ -1,16 +1,23 @@
 package com.firen.awesomeblocks;
 
+import java.util.Collections;
 import java.util.Random;
+
+import android.util.Log;
 
 public class Board {
 	private int tiles[][];
+	private boolean selectedTiles[][];
 	private int pallete[];
 
 	public Board(int width, int height, int pallete[]) {
 		tiles = new int[height][];
+		selectedTiles = new boolean[height][];
 		for (int i = 0; i < height; i++) {
 			tiles[i] = new int[width];
+			selectedTiles[i] = new boolean[width];
 		}
+
 		this.pallete = pallete;
 	}
 
@@ -21,17 +28,25 @@ public class Board {
 	public int getTilePalleteIndex(int x, int y) {
 		return this.tiles[y][x];
 	}
-	
-	public int getTileColor( int x, int y ) {
+
+	public int getTileColor(int x, int y) {
 		return this.pallete[this.tiles[y][x]];
 	}
-	
+
 	public int getWidth() {
 		return this.tiles[0].length;
 	}
-	
+
 	public int getHeight() {
 		return this.tiles.length;
+	}
+
+	public void setSelected(int x, int y, boolean selected) {
+		this.selectedTiles[y][x] = selected;
+	}
+
+	public boolean isSelected(int x, int y) {
+		return this.selectedTiles[y][x];
 	}
 
 	static void generateBoardTiles(Board board) {
--- src/com/firen/awesomeblocks/GameplaySurface.java
+++ src/com/firen/awesomeblocks/GameplaySurface.java
@@ -4,15 +4,18 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Point;
 import android.graphics.Rect;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class GameplaySurface extends View {
 
 	private Board board;
 
-	public static final int BLOCK_WIDTH = 10;
-	public static final int BLOCK_HEIGHT = 10;
+	public static final int BLOCK_WIDTH = 50;
+	public static final int BLOCK_HEIGHT = 50;
 
 	public GameplaySurface(Context context) {
 		super(context);
@@ -28,7 +31,17 @@ public class GameplaySurface extends View {
 			for (int i = 0; i < this.board.getHeight(); i++) {
 				for (int j = 0; j < this.board.getWidth(); j++) {
 					p.setColor(this.board.getTileColor(j, i));
-					canvas.drawRect(j*BLOCK_WIDTH, i*BLOCK_HEIGHT, j*BLOCK_WIDTH+BLOCK_WIDTH, i*BLOCK_HEIGHT+BLOCK_HEIGHT, p);
+					p.setStyle(Paint.Style.FILL);
+					canvas.drawRect(j * BLOCK_WIDTH, i * BLOCK_HEIGHT, j
+							* BLOCK_WIDTH + BLOCK_WIDTH, i * BLOCK_HEIGHT
+							+ BLOCK_HEIGHT, p);
+					if (this.board.isSelected(j, i)) {
+						p.setColor(Color.RED);
+						p.setStyle(Paint.Style.STROKE);
+						canvas.drawRect(j * BLOCK_WIDTH + 1, i * BLOCK_HEIGHT
+								+ 1, j * BLOCK_WIDTH + BLOCK_WIDTH - 2, i
+								* BLOCK_HEIGHT + BLOCK_HEIGHT - 2, p);
+					}
 				}
 			}
 		}
@@ -40,4 +53,64 @@ public class GameplaySurface extends View {
 		drawBoard(canvas);
 	}
 
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		super.onTouchEvent(event);
+		if (event.getAction() == MotionEvent.ACTION_UP) {
+			Point p = getTileLocation((int) event.getX(), (int) event.getY());
+			if (p.x < this.board.getWidth() && p.y < this.board.getHeight()) {
+				this.board.setSelected(p.x, p.y, true);
+				selectNeighbourBlocks(p.x, p.y);
+				invalidate();
+			}
+		}
+		return true;
+	}
+
+	private boolean isPointInTheBoard(int x, int y) {
+		return !(x < 0 || x >= this.board.getWidth() || y < 0 || y >= this.board
+				.getHeight());
+	}
+
+	private boolean isPointThatSameColor(int x1, int y1, int x2, int y2) {
+		if (isPointInTheBoard(x1, y1) && isPointInTheBoard(x2, y2)) {
+			return this.board.getTileColor(x1, y1) == this.board.getTileColor(
+					x2, y2);
+		} else {
+			return false;
+		}
+	}
+
+	private boolean isPointSelected(int x, int y) {
+		return isPointInTheBoard(x, y) && this.board.isSelected(x, y);
+	}
+
+	private void selectNeighbourBlocks(int x, int y) {
+		if (isPointInTheBoard(x, y)) {
+			if (isPointThatSameColor(x, y, x - 1, y)
+					&& !isPointSelected(x - 1, y)) {
+				this.board.setSelected(x - 1, y, true);
+				selectNeighbourBlocks(x - 1, y);
+			}
+			if (isPointThatSameColor(x, y, x, y - 1)
+					&& !isPointSelected(x, y - 1)) {
+				this.board.setSelected(x, y - 1, true);
+				selectNeighbourBlocks(x, y - 1);
+			}
+			if (isPointThatSameColor(x, y, x + 1, y)
+					&& !isPointSelected(x + 1, y)) {
+				this.board.setSelected(x + 1, y, true);
+				selectNeighbourBlocks(x + 1, y);
+			}
+			if (isPointThatSameColor(x, y, x, y + 1)
+					&& !isPointSelected(x, y + 1)) {
+				this.board.setSelected(x, y + 1, true);
+				selectNeighbourBlocks(x, y + 1);
+			}
+		}
+	}
+
+	private Point getTileLocation(int x, int y) {
+		return new Point(x / BLOCK_WIDTH, y / BLOCK_HEIGHT);
+	}
 }

d727b3f86220e87e33d655ce7aa510206deff2a3
firen@firen-PC <none@none>
2011-10-11 00:07:52 +0200

    add simple protection against accidentially pressing back button

--- src/com/firen/awesomeblocks/GameplayActivity.java
+++ src/com/firen/awesomeblocks/GameplayActivity.java
@@ -15,20 +15,38 @@ public class GameplayActivity extends Activity {
 		setContentView(new GameplaySurface(this, this.board));
 	}
 
-//	@Override
-//	public void onBackPressed() {
-//		super.onBackPressed();
-//		AlertDialog alert = new AlertDialog.Builder(this).create();
-//		alert.setTitle("SURE?");
-//		alert.show();
-//		alert.setButton("YES", new OnClickListener() {
-//			
-//			@Override
-//			public void onClick(DialogInterface dialog, int which) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//		});
-//	}
-	
+	class AlertOnClickListener implements DialogInterface.OnClickListener {
+
+		private Activity activity;
+
+		public AlertOnClickListener(Activity activity) {
+			this.activity = activity;
+		}
+
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			switch (which) {
+			case DialogInterface.BUTTON_POSITIVE:
+				this.activity.finish();
+				break;
+			case DialogInterface.BUTTON_NEGATIVE:
+				dialog.cancel();
+				break;
+			}
+		}
+	}
+
+	@Override
+	public void onBackPressed() {
+		AlertDialog alert = new AlertDialog.Builder(this).create();
+		alert.setTitle("SURE?");
+		AlertOnClickListener alertOnClickListener = new AlertOnClickListener(
+				this);
+		alert.setButton(DialogInterface.BUTTON_POSITIVE, "Yes",
+				alertOnClickListener);
+		alert.setButton(DialogInterface.BUTTON_NEGATIVE, "No",
+				alertOnClickListener);
+		alert.show();
+	}
+
 }

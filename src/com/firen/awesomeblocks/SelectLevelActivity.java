1b368d6baa94fa070ba283d99740bce1e8f9d254
firen@firen-PC <none@none>
2011-10-22 22:15:05 +0200

    add levels menu creation based on level files in the assets, add theme
    --HG--
    rename : assets/level1.lvl => assets/levels/level1.lvl
    rename : assets/level2.lvl => assets/levels/level2.lvl

--- src/com/firen/awesomeblocks/SelectLevelActivity.java
+++ src/com/firen/awesomeblocks/SelectLevelActivity.java
@@ -1,15 +1,20 @@
 package com.firen.awesomeblocks;
 
 import java.io.IOException;
+import java.util.List;
 
 import android.app.Activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
 
 public class SelectLevelActivity extends Activity {
 
@@ -19,37 +24,13 @@ public class SelectLevelActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.level_select_layout);
-		Button button = (Button) findViewById(R.id.level1);
+
+		GridView levelsGridView = (GridView) findViewById(R.id.levelsGridView);
+		levelsGridView.setAdapter(new LevelsAdapter(this));
+
+		Button button = (Button) findViewById(R.id.level_select_back);
 		button.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				try {
-					getGamePlayActivity().putExtra("board", Board.createBoard(getAssets().open("level1.lvl")));
-				} catch (IOException e) {
-					Log.e(SelectLevelActivity.class.getSimpleName(), "level1.lvl cannot be loaded", e);
-				}
-				startActivity(getGamePlayActivity());
-			}
-		});
-		
-		button = (Button) findViewById(R.id.level2);
-		button.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				try {
-					getGamePlayActivity().putExtra("board", Board.createBoard(getAssets().open("level2.lvl")));
-				} catch (IOException e) {
-					Log.e(SelectLevelActivity.class.getSimpleName(), "level2.lvl cannot be loaded", e);
-				}
-				startActivity(getGamePlayActivity());
-			}
-		});
-		
-		button = (Button) findViewById(R.id.level_select_back);
-		button.setOnClickListener(new OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
 				finish();
@@ -57,10 +38,91 @@ public class SelectLevelActivity extends Activity {
 		});
 	}
 
+	private String[] getStages() {
+		try {
+			return getAssets().list("levels");
+		} catch (IOException e) {
+			Log.e(SelectLevelActivity.class.getSimpleName(),
+					"can not get list of stages from assets", e);
+			return null;
+		}
+	}
+
 	private Intent getGamePlayActivity() {
 		if (this.gamePlayActivity == null) {
 			this.gamePlayActivity = new Intent(this, GameplayActivity.class);
 		}
 		return gamePlayActivity;
+	}
+
+	private class LevelsAdapter extends BaseAdapter {
+
+		private Context context;
+
+		public LevelsAdapter(Context context) {
+			this.context = context;
+		}
+
+		@Override
+		public int getCount() {
+			try {
+				return getAssets().list("levels").length;
+			} catch (IOException e) {
+				Log.e(LevelsAdapter.class.getSimpleName(),
+						"Cannot get levels count");
+				return 0;
+			}
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			Button levelButton;
+			if (convertView == null) {
+				levelButton = new Button(context);
+				levelButton.setText("Level" + (position + 1));
+				levelButton.setOnClickListener(new LevelOnClickListener(
+						position + 1));
+				return levelButton;
+			} else {
+				return convertView;
+			}
+		}
+
+	}
+
+	private class LevelOnClickListener implements View.OnClickListener {
+
+		private int level;
+
+		public LevelOnClickListener(int level) {
+			this.level = level;
+		}
+
+		@Override
+		public void onClick(View v) {
+			try {
+				getGamePlayActivity().putExtra(
+						"board",
+						Board.createBoard(getAssets().open(
+								"levels/" + "level" + level + ".lvl")));
+			} catch (IOException e) {
+				Log.e(SelectLevelActivity.class.getSimpleName(),
+						"level" + level + ".lvl cannot be loaded", e);
+			}
+			startActivity(getGamePlayActivity());
+		}
+
 	}
 }
